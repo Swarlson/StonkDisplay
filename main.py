3ae459e97a98e3e71bb398c3b017b81241dcae4b
Danny <daniel.goldfayn@gmx.de>
2021-03-08 16:12:13 +0100

    First Branch Update

--- main.py
+++ main.py
@@ -3,35 +3,48 @@ import os
 import json
 from req_lib import Message
 import signal
+from I2C_LCD_driver import i2c_device, lcd
 
 
 #todo: Display Time and only update if time changes (maybe)
 investment = 393.21
 n_coins = 322
+content = "Foobar"
 
 def evaluate_patrik(rate, investment, n_coins):
     dif = round(float(rate)*n_coins - investment, 2)
     if dif < 0:
-        pepelaugh(dif)
+        content = pepelaugh(dif)
     else:
-        pogChamp(dif)
+        content = pogChamp(dif)
+
+    return content
 
 
 def pepelaugh(dif):
     print("Pepelaugh ->{}€".format(dif))
+    return content
 
 def pogChamp(dif):
     print("PogChamp ->{}€".format(dif))
+    return content
 
 if __name__ == "__main__":
 
     msg = Message("https://api.coingecko.com/api/v3/")
+    mylcd = lcd()
+    mylcd.start()
     #resp = msg.request_only_price('enjincoin', 'eur')
-    #evaluate_patrik(resp,investment,n_coins)S
+    #evaluate_patrik(resp,investment,n_coins)
     try:
         while True:
             resp = msg.request_only_price('enjincoin', 'eur')
-            evaluate_patrik(resp,investment,n_coins)       
+            content = evaluate_patrik(resp,investment,n_coins)       
+            mylcd.update_buffer("Patriks Stonks",1,0)
+            mylcd.update_buffer(content, 2, 0)
+
+            mylcd.update_buffer("Screen2",1,1)
+            mylcd.update_buffer("Hodl",2, 1)
             sleep(60) 
     except KeyboardInterrupt:
         print('Ending the program')
